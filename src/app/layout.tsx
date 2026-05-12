caf0dcab6ac7a3a5052c47c5fc9a1766d08cb01d
knnseleznov <knnseleznov@gmail.com>
2026-05-12 18:55:45 +0100

    chore: add global styles & fonts & layout file

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Providers } from "./providers";
+import { geistMono, geistSans } from "./fonts";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Crash Game",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
